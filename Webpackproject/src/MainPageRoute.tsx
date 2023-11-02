ac3db077d42d3240ca6fdc58b325af252adee767
fangfelipe <fangfelipe@gmail.com>
2023-11-02 13:33:27 +0800

    update webpack size issue

--- Webpackproject/src/MainPageRoute.tsx
+++ Webpackproject/src/MainPageRoute.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 import ErrorPage from './ErrorElement';
-import moment from "moment";
 
 function MainPage() {
-  console.log(moment("20111031", "YYYYMMDD").fromNow())
   return (
     <div >
       <Link style={{ padding: "10px" }} to="/xlsx">XlsxPage</Link>
--- Webpackproject/webpack.config.js
+++ Webpackproject/webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
         splitChunks: {
             // minSize: 1,
             // chunks: 'all',
+            maxSize: 250000,
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
